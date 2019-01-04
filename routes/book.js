a35de78b0b1260fd68ccd63624108c414e225bd6
josei <joseice@gmail.com>
2019-01-04 00:06:50 -0500

    fix Book to book in model

--- routes/book.js
+++ routes/book.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router([]);
 const mongoose = require('mongoose');
-const Book = require('../models/Book.js');
+const Book = require('../models/book.js');
 const cors = require('cors');
 
 router.options('*', cors());
